6e8121031898b84d14df1478a9a2b93c697f67e2
Francisco Baio Dias <xicombd@gmail.com>
2016-03-05 21:03:15 +0000

    Initial commit :rocket:

---
+++ test/index.js
@@ -0,0 +1,25 @@
+const freezify = require('..')
+const fs = require('fs-blob-store')
+
+const ORIGIN_PATH = __dirname + '/example'
+const DEST_PATH = __dirname + '/dest'
+
+const options = {
+  origin: {
+    store: fs(ORIGIN_PATH),
+    path: ORIGIN_PATH
+  },
+  destination: {
+    store: fs(DEST_PATH),
+    path: DEST_PATH
+  }
+}
+
+freezify('index.html', options, function (err) {
+  if (err) {
+    console.log(err)
+    return
+  }
+
+  console.log('all done')
+})
